b236a541577131c12f1db79392aea100137fabce
Piet Delport <pjdelport@gmail.com>
2015-03-06 22:48:01 +0200

    ssh-config-dir: Simplify the config-diff.sh script
    I don't know why I didn't just use diff -N to begin with.

--- ssh-config-dir/.ssh/config-diff.sh
+++ ssh-config-dir/.ssh/config-diff.sh
@@ -1,11 +1,6 @@
 #!/bin/sh -e
 # Compare ~/.ssh/config to ~/.ssh/config.d/*.conf
 
-if [ -f ~/.ssh/config ]; then
-    if cat ~/.ssh/config.d/*.conf | diff -u ~/.ssh/config -; then
-        echo '~/.ssh/config is up to date.'
-    fi
-else
-    echo "$(basename "$0"): ~/.ssh/config does not exist! Showing built config instead." >&2
-    cat ~/.ssh/config.d/*.conf
+if cat ~/.ssh/config.d/*.conf | diff -Nu ~/.ssh/config -; then
+    echo '~/.ssh/config is up to date.'
 fi
