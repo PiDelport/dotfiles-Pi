ffb2e7b86d2d492aac884f43f80c893193c0b30f
Piet Delport <pjdelport@gmail.com>
2015-07-09 16:50:01 +0200

    python-virtualenvwrapper: Remove old ~/.cache/pip-wheelhouse usage
    Between pip 7 wheel caching and a postmkvirtualenv hook, I no longer use this.

--- python-virtualenvwrapper/.bashrc.d/python-virtualenvwrapper.sh
+++ python-virtualenvwrapper/.bashrc.d/python-virtualenvwrapper.sh
@@ -3,17 +3,6 @@
 # See http://virtualenvwrapper.readthedocs.org/
 
 
-# Use a local package directory to find up-to-date versions of setuptools and
-# pip when creating virtualenvs.
-#
-# For convenience, ~/.cache/pip-wheelhouse should match the wheel-dir
-# configured in pip.conf.
-#
-alias mkproject='mkproject --extra-search-dir="$HOME/.cache/pip-wheelhouse"'
-alias mktmpenv='mktmpenv --extra-search-dir="$HOME/.cache/pip-wheelhouse"'
-alias mkvirtualenv='mkvirtualenv --extra-search-dir="$HOME/.cache/pip-wheelhouse"'
-
-
 # Python 3 variants of the usual virtualenv-creating commands.
 #
 alias mkproject3='mkproject --python="$(which python3)"'
