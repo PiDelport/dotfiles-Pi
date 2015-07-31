4a7fe630ec901c0e740b5ca16755859043827540
Piet Delport <pjdelport@gmail.com>
2015-07-31 13:35:28 +0200

    python-django: Building lxml requires libz-dev too

--- python-django/.bashrc.d/python-django-helpers.sh
+++ python-django/.bashrc.d/python-django-helpers.sh
@@ -18,7 +18,7 @@ ___django_add_superuser () {
 
 # Install various sets of system package dependencies.
 ___install_python_deps () { sudo aptitude install python-dev python3-dev; }
-___install_lxml_deps () { sudo aptitude install libxml2-dev libxslt-dev; }
+___install_lxml_deps () { sudo aptitude install libz-dev libxml2-dev libxslt-dev; }
 ___install_pillow_deps () { sudo aptitude install zlib1g-dev libjpeg-dev libwebp-dev liblcms2-dev libfreetype6-dev; }
 ___install_postgresql_deps () { sudo aptitude install libpq-dev; }
 ___install_geodjango_deps () { sudo aptitude install libgeos-dev libgeos++-dev libproj-dev libgdal-dev; }
