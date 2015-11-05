0cf61874a9f69dcb576eb12708f7251ee926245e
Piet Delport <pjdelport@gmail.com>
2015-11-05 21:29:53 +0200

    haskell-stack: Add ghci -> stack ghci fallback

--- haskell-stack/.bashrc.d/haskell-stack.sh
+++ haskell-stack/.bashrc.d/haskell-stack.sh
@@ -3,4 +3,13 @@ if which stack >/dev/null; then
     # https://github.com/commercialhaskell/stack/blob/master/doc/shell_autocompletion.md
     eval "$(stack --bash-completion-script "$(which stack)")"
 
+    # If there's no ghci on the path, define a wrapper to invoke stack ghci instead.
+    if ! which ghci >/dev/null; then
+        # This is a bit hacky, but can run with or without options to ghci.
+        ghci () {
+            # Note: Intentionally uses * instead of @.
+            stack ghci ${*:+--ghc-options "$*"}
+        }
+    fi
+
 fi
