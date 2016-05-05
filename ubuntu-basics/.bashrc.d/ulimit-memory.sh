c38592402438147942cf1964ed9226bed0a87d2a
Piët Delport <pjdelport@gmail.com>
2016-05-05 13:36:51 +0200

    ubuntu-basics: Add a quick&dirty virtual memory ulimit helper function

--- ubuntu-basics/.bashrc.d/ulimit-memory.sh
+++ ubuntu-basics/.bashrc.d/ulimit-memory.sh
@@ -1,3 +1,8 @@
 # Soft-limit the default maximum virtual memory to 1 GiB (2^20 KiB)
 # to soften the blow from accidental runaway processes.
 ulimit -S -v $((2**20))
+
+# Helper: Set the virtual memory limit (in GiB), or show it (in KiB)
+___ulimit_virtual () {
+    ulimit -S -v ${1:+"$(( $1 * 2**20 ))"}
+}
