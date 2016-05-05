73cbf35e44fa339f67d524bdf60a5e2db48572d2
Piët Delport <pjdelport@gmail.com>
2016-05-05 17:23:13 +0200

    (ubuntu-basics: Refactor)

--- ubuntu-basics/.bashrc.d/ulimit-memory.sh
+++ ubuntu-basics/.bashrc.d/ulimit-memory.sh
@@ -1,8 +1,9 @@
-# Soft-limit the default maximum virtual memory to 1 GiB (2^20 KiB)
-# to soften the blow from accidental runaway processes.
-ulimit -S -v $((2**20))
-
 # Helper: Set the virtual memory limit (in GiB), or show it (in KiB)
 ___ulimit_virtual () {
+    # 1 GiB = 2^20 KiB
     ulimit -S -v ${1:+"$(( $1 * 2**20 ))"}
 }
+
+# Soft-limit the default maximum virtual memory to 1 GiB
+# to soften the blow from accidental runaway processes.
+___ulimit_virtual 1
