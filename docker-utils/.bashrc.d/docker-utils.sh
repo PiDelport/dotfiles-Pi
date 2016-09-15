d3848a53d0bf280be9d724cfca0f5bc6dccd43ed
Pi Delport <pjdelport@gmail.com>
2016-09-15 17:54:07 +0200

    docker-utils: Add comman to just list the stale items too

--- docker-utils/.bashrc.d/docker-utils.sh
+++ docker-utils/.bashrc.d/docker-utils.sh
@@ -1,8 +1,19 @@
 # Docker utilities
 
 
-# Remove exited containers and dangling images.
-___docker_cleanup () {
+# List stale containers, images, and volumes.
+___docker_list_stale () {
+    docker ps -f status=exited
+    echo
+    docker images -f dangling=true
+    echo
+    docker volume ls -f dangling=true
+    echo
+}
+
+
+# Remove stale containers, images, and volumes.
+___docker_cleanup_stale () {
     docker rm -v $(docker ps -q -f status=exited)
     docker rmi $(docker images -q -f dangling=true)
     docker volume rm $(docker volume ls -q -f dangling=true)
