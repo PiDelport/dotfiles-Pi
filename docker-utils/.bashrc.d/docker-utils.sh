760f986e849d22ccd594f0bdd9bc251fc7a508b5
Pi Delport <pjdelport@gmail.com>
2016-09-15 17:53:32 +0200

    docker-utils: Clean up dangling volumes too

--- docker-utils/.bashrc.d/docker-utils.sh
+++ docker-utils/.bashrc.d/docker-utils.sh
@@ -5,4 +5,5 @@
 ___docker_cleanup () {
     docker rm -v $(docker ps -q -f status=exited)
     docker rmi $(docker images -q -f dangling=true)
+    docker volume rm $(docker volume ls -q -f dangling=true)
 }
