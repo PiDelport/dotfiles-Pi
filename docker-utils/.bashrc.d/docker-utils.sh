990447a77376e24245444ec967de357e9c03f443
Pi Delport <pjdelport@gmail.com>
2016-10-28 17:45:09 +0200

    docker-utils: Add ___docker_image_manifest, ___docker_image_diff

--- docker-utils/.bashrc.d/docker-utils.sh
+++ docker-utils/.bashrc.d/docker-utils.sh
@@ -18,3 +18,30 @@ ___docker_stale_cleanup () {
     docker rmi $(docker images -q -f dangling=true)
     docker volume rm $(docker volume ls -q -f dangling=true)
 }
+
+
+# Show a filesystem manifest for an image.
+___docker_image_manifest () {
+    local usage="Usage: ___docker_image_manifest IMAGE"
+    local image="${1:?$usage}"
+
+    # This is just a find invocation, so it won't work on images without find.
+    #
+    # The -mount option prevents find from descending into special mounts such
+    # as /proc and /sys, but should still list everything we're interested in.
+    #
+    docker run --rm "$image" find / -mount
+}
+
+# Show a filesystem diff between two images.
+#
+# The optional third argument overrides the diff command to invoke.
+# Examples: 'diff -u', vimdiff, meld
+#
+___docker_image_diff () {
+    local usage="Usage: ___docker_image_diff IMAGE1 IMAGE2 [diff command]"
+    local image1="${1:?$usage}"
+    local image2="${2:?$usage}"
+    local diff_command="${3:-diff}"
+    $diff_command <(___docker_image_manifest "$image1" | sort) <(___docker_image_manifest "$image2" | sort)
+}
