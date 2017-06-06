ba934d165307a19024e6c19114f5d8ef3cc0f932
Pi Delport <pjdelport@gmail.com>
2017-06-06 12:17:21 +0200

    python-virtualenvwrapper: Add Python 3.6 to version list

--- python-virtualenvwrapper/.bashrc.d/python-virtualenvwrapper.sh
+++ python-virtualenvwrapper/.bashrc.d/python-virtualenvwrapper.sh
@@ -5,7 +5,7 @@
 
 # Versioned variants of the usual virtualenv-creating commands.
 #
-for ver in 2 3 2.7 3.4 3.5; do
+for ver in 2 3 2.7 3.4 3.5 3.6; do
     if test -x "$(which python${ver})"; then
         alias mkproject${ver}='mkproject --python="$(which python'${ver}')"'
         alias mktmpenv${ver}='mktmpenv --python="$(which python'${ver}')"'
