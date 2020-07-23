76d9397640cb53feef9f847079e7144984e932cc
Pi Delport <pjdelport@gmail.com>
2020-07-23 09:31:28 +0200

    python-virtualenvwrapper: Update Python version list (drop 3.4, add 3.7–10)

--- python-virtualenvwrapper/.bashrc.d/python-virtualenvwrapper.sh
+++ python-virtualenvwrapper/.bashrc.d/python-virtualenvwrapper.sh
@@ -5,7 +5,7 @@
 
 # Versioned variants of the usual virtualenv-creating commands.
 #
-for ver in 2 3 2.7 3.4 3.5 3.6; do
+for ver in 2{,.7} 3{,.{5,6,7,8,9,10}}; do
     if test -x "$(which python${ver})"; then
         alias mkproject${ver}='mkproject --python="$(which python'${ver}')"'
         alias mktmpenv${ver}='mktmpenv --python="$(which python'${ver}')"'
