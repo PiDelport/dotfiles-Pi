c9a57270311445210914d0146f3e6d5167d6c63d
Pi Delport <pjdelport@gmail.com>
2021-02-10 11:43:38 +0200

    feat(git-common): add bash alias: g = git
    Also enable completion.

--- git-common/.bashrc.d/git-common.sh
+++ git-common/.bashrc.d/git-common.sh
@@ -1,3 +1,9 @@
+alias g=git
+# Re-use the git command's completion.
+# See /usr/share/bash-completion/completions/git (or similar)
+__load_completion git && __git_complete g __git_main
+
+
 ___git_pjdelport@gmail.com () {
     git config --local user.name 'Pi Delport'
     git config --local user.email 'pjdelport@gmail.com'
