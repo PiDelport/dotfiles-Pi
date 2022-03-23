780a4edee543c0670f14dac14881c03c31c53524
Pi Delport <pjdelport@gmail.com>
2022-03-23 19:09:06 +0200

    feat(ubuntu-basics): disable default ulimit
    Too many things don't work, with this enabled.
    
    earlyoom or oomd are probably better overall solutions.

--- ubuntu-basics/.bashrc.d/ulimit-memory.sh
+++ ubuntu-basics/.bashrc.d/ulimit-memory.sh
@@ -3,7 +3,3 @@ ___ulimit_virtual () {
     # 1 GiB = 2^20 KiB
     ulimit -S -v ${1:+"$(( $1 * 2**20 ))"}
 }
-
-# Soft-limit the default maximum virtual memory to 4 GiB
-# to soften the blow from accidental runaway processes.
-___ulimit_virtual 4
