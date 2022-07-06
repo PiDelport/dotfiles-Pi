0914cde0af2733073d4a02fea547a113b74928d8
Pi Delport <pjdelport@gmail.com>
2022-07-07 00:15:18 +0200

    feat(laptop-utils): add dell-bd-prochot

---
+++ laptop-utils/.bashrc.d/dell-bd-prochot.sh
@@ -0,0 +1,43 @@
+# Helpers to deal with Dell laptops throttling CPU with non-OEM power supplies.
+#
+# Background:
+#
+# - https://superuser.com/questions/1160735/stop-dell-from-throttling-cpu-with-power-adapter
+# - https://en.wikipedia.org/wiki/Model-specific_register
+#
+# Requires msr-tools:
+#
+#       sudo apt install msr-tools
+
+___bd_prochot () (
+    local -
+    set -euo pipefail
+    sudo -v
+    local msr="0x$(sudo rdmsr 0x1FC)"
+    local msr_bd_prochot_disabled="$(printf '0x%x\n' "$(( "${msr}" & 0xFFFFFFFE ))")"
+    local msr_bd_prochot_enabled="$(printf '0x%x\n' "$(( "${msr}" | 0x1 ))")"
+    echo "Current MSR: ${msr}"
+    echo
+    echo "To disable BD PROCHOT:"
+    echo
+    echo "sudo wrmsr ${msr_bd_prochot_disabled}"
+    echo
+    echo "To enable BD PROCHOT:"
+    echo
+    echo "sudo wrmsr ${msr_bd_prochot_enabled}"
+    echo
+)
+
+___disable_bd_prochot () (
+    local -
+    set -euo pipefail
+    sudo -v
+    local msr="0x$(sudo rdmsr 0x1FC)"
+    local msr_bd_prochot_disabled="$(printf '0x%x\n' "$(( "${msr}" & 0xFFFFFFFE ))")"
+    sudo wrmsr 0x1FC "${msr_bd_prochot_disabled}"
+    echo "Disabled BD PROCHOT; MSR value: ${msr} → ${msr_bd_prochot_disabled}"
+)
+
+# As a dash-compatible one-liner for /etc/rc./local:
+#
+# wrmsr 0x1FC "$(printf '0x%x\n' "$(( 0x$(rdmsr 0x1FC) & 0xFFFFFFFE ))")"
